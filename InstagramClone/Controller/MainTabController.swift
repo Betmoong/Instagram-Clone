4b2abd71317865f325189ee9d32e7f9bd7a308e3
Betmoong <rdev99@naver.com>
2023-01-18 04:16:42 +0900

    [ADD] MainTabController

---
+++ InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/01/18.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
