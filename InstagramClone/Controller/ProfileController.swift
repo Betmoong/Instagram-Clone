5c98b976bf30daef82a1d4f216449aedec8eeeac
Betmoong <rdev99@naver.com>
2023-01-18 16:04:03 +0900

    [ADD] Controllers 추가

---
+++ InstagramClone/Controller/ProfileController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileController.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/01/18.
+//
+
+import Foundation
---
+++ InstagramClone/Controller/SearchController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchController.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/01/18.
+//
+
+import Foundation
