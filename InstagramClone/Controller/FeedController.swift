3d9b92f6ae7e6cef466f5457325e4f755cd898f7
Betmoong <rdev99@naver.com>
2023-01-18 16:07:23 +0900

    [FEAT] TabBar

--- InstagramClone/Controller/FeedController.swift
+++ InstagramClone/Controller/FeedController.swift
@@ -5,4 +5,12 @@
 //  Created by Rocky on 2023/01/18.
 //
 
-import Foundation
+import UIKit
+
+class FeedController: UIViewController {
+    override func viewDidLoad() {
+        
+        view.backgroundColor = .systemMint
+    }
+    
+}
