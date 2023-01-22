5a391a620daebe7cb8327a0ee2be4f9d4ccfa73e
Betmoong <rdev99@naver.com>
2023-01-23 04:25:56 +0900

    [FEAT] LoginPage UI 구현

---
+++ InstagramClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/01/23.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
