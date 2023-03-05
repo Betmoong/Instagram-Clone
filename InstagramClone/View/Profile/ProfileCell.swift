5bcb8ec7b21c5e67ef6c09dd1c648b6e72fd6e5e
Betmoong <rdev99@naver.com>
2023-03-05 23:46:05 +0900

    [Feat] Profile UI

---
+++ InstagramClone/View/Profile/ProfileCell.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileCell.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/03/05.
+//
+
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "simple_macbook_M1_pro")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .lightGray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
