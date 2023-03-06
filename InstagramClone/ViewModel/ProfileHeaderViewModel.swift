93027f79dc38e0414365461ae8cf6d56a7542516
Betmoong <rdev99@naver.com>
2023-03-06 13:30:24 +0900

    [Feat] 가져온 User Data로 Profile Header 구현

---
+++ InstagramClone/ViewModel/ProfileHeaderViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeaderViewModel.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/03/06.
+//
+
+import Foundation
+
+struct ProfileHeaderViewModel {
+    let user: User
+    
+    var fullname: String {
+        return user.fullname
+    }
+    
+    var profileImageUrl: URL? {
+        return URL(string: user.profileImageUrl)
+    }
+    
+    init(user: User) {
+        self.user = user
+    }
+}
