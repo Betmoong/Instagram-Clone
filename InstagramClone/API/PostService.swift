4d9aa1e4084e61282a47d0ddd620fc08647ff4c1
Betmoong <rdev99@naver.com>
2023-03-09 19:12:22 +0900

    [FEAT] Firebase Storage에 게시물 업로드

---
+++ InstagramClone/API/PostService.swift
@@ -0,0 +1,26 @@
+//
+//  PostService.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/03/09.
+//
+
+import UIKit
+import Firebase
+
+struct PostService {
+    
+    static func uploadPost(caption: String, image: UIImage, completion: @escaping(FirestoreCompletion)) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        ImageUploader.uploadImage(image: image) { imageUrl in
+            let data = ["caption": caption,
+                        "timestamp": Timestamp(date: Date()),
+                        "likes": 0,
+                        "imageUrl": imageUrl,
+                        "ownerUid": uid] as [String : Any]
+            
+            COLLECTION_POSTS.addDocument(data: data, completion: completion)
+        }
+    }
+}
