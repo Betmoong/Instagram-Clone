5c59f6c612e9d6f86cf8824c78fc3b05089fee89
Betmoong <rdev99@naver.com>
2023-03-12 20:03:32 +0900

    [FEAT] 게시물 피드에 Refresher 새로고침 추가

---
+++ InstagramClone/ViewModel/PostViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewModel.swift
+//  InstagramClone
+//
+//  Created by Rocky on 2023/03/12.
+//
+
+import Foundation
+
+struct PostViewModel {
+    private let post: Post
+    
+    var imageUrl: URL? { return URL(string: post.imageUrl) }
+    
+    var userProfileImageUrl: URL? { return URL(string: post.ownerImageUrl) }
+    
+    var username: String { return post.ownerUsername }
+    
+    var caption: String { return post.caption }
+    
+    var likes: Int { return post.likes }
+    
+    var likesLabelText: String {
+        return "좋아요 \(post.likes)개"
+    }
+    
+    init(post: Post) {
+        self.post = post
+    }
+}
