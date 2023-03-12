e885431e22b6c71a69c7ea70862d35d419be14c7
Betmoong <rdev99@naver.com>
2023-03-12 22:20:51 +0900

    [FEAT] 사용자 프로필 게시물 구현

--- InstagramClone/View/Profile/ProfileCell.swift
+++ InstagramClone/View/Profile/ProfileCell.swift
@@ -11,6 +11,10 @@ class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    var viewModel: PostViewModel? {
+        didSet{ configure() }
+    }
+    
     private let postImageView: UIImageView = {
         let iv = UIImageView()
         iv.image = #imageLiteral(resourceName: "simple_macbook_M1_pro")
@@ -32,6 +36,12 @@ class ProfileCell: UICollectionViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        postImageView.sd_setImage(with: viewModel.imageUrl)
     }
 }
 
